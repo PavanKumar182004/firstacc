015634095c995eb459ec3e4deb67f622ccb31a49
Pavan kumar <gandhepavankumar@gmail.com>
2025-02-03 15:01:15 +0530

    JavaDoc

---
+++ src/code/JavaDocExample.java
@@ -0,0 +1,16 @@
+package code;
+
+/** This is my first doc  document
+ * @author pavanKumar
+ */
+public class JavaDocExample {
+    /**
+     *
+     * @param string
+     * @param number
+     * @return
+     */
+    public String sampleMethod(String string, int number) {
+        return "Result";
+    }
+}
